88041d3cd6763666e839fbc0642a2aaddaccb296
MacBook Pro 13 <rahmah@sprintive.com>
2021-03-21 22:35:13 +0200

    refactoring the template

---
+++ navigation/RootNavigator.tsx
@@ -0,0 +1,21 @@
+import { createStackNavigator } from '@react-navigation/stack'
+import React from 'react'
+import { StyleSheet } from 'react-native'
+import NotFoundScreen from '../screens/NotFoundScreen'
+import BottomTabNavigator from './BottomTabNavigator'
+const Stack = createStackNavigator<RootStackParamList>()
+
+export default function RootNavigator () {
+  return (
+    <Stack.Navigator screenOptions={{ headerShown: false }}>
+      <Stack.Screen name='Root' component={BottomTabNavigator} />
+      <Stack.Screen
+        name='NotFound'
+        component={NotFoundScreen}
+        options={{ title: 'Oops!' }}
+      />
+    </Stack.Navigator>
+  )
+}
+
+const styles = StyleSheet.create({})
--- screens/NotFoundScreen.tsx
+++ screens/NotFoundScreen.tsx
@@ -2,7 +2,6 @@ import { StackScreenProps } from '@react-navigation/stack';
 import * as React from 'react';
 import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
-import { RootStackParamList } from '../types';
 
 export default function NotFoundScreen({
   navigation,
